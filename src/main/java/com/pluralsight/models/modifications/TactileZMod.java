349005e01750faf16fd64dabc72bfdce1bf44b66
Louie D <louie.delrosario97@gmail.com>
2026-05-25 15:13:47 -0700

    Created class w/ class info

--- src/main/java/com/pluralsight/models/modifications/TactileZMod.java
+++ src/main/java/com/pluralsight/models/modifications/TactileZMod.java
@@ -1,5 +1,17 @@
 package com.pluralsight.models.modifications;
 
-public class TactileZMod {
+public class TactileZMod extends Mod
+{
+    // No boolean variable needed. We will store all mods in List<Mod>
+    // This will allow us to make changes w/o needing to change this class.
+
+    // Pricing Variables
+    private static final double price = 30.00;
+
+    public TactileZMod()
+    { super("Tactile Z Button"); }
+
+    @Override
+    public double getPrice() { return price; }
 }
 
