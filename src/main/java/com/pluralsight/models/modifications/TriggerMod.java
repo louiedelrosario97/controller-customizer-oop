f4d7a77babe5ca3bc6ce1af1dc88b2d2a365d717
Louie D <louie.delrosario97@gmail.com>
2026-05-25 15:14:16 -0700

    Created class w/ getPrice() logic

--- src/main/java/com/pluralsight/models/modifications/TriggerMod.java
+++ src/main/java/com/pluralsight/models/modifications/TriggerMod.java
@@ -1,5 +1,35 @@
 package com.pluralsight.models.modifications;
 
-public class TriggerMod {
+public class TriggerMod extends Mod
+{
+    // Trigger Mod Characteristics
+    private final String side;
+
+    // Pricing variables
+    private static final double leftPrice = 10.00;
+    private static final double rightPrice = 10.00;
+    private static final double bothPrice = 20.00;
+
+    // Constructor
+    public TriggerMod(String side)
+    {
+        super(side);
+        this.side = side;
+    }
+
+    // Getter
+    public String getSide() { return side; }
+
+    @Override
+    public double getPrice()
+    {
+        return switch (side.toLowerCase())
+        {
+            case "left" -> leftPrice;
+            case "right" -> rightPrice;
+            case "both" -> bothPrice;
+            default -> 0.00;
+        };
+    }
 }
 
