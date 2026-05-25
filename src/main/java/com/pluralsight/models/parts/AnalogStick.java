85a74961cfab4f3ce1b3d934662bfaa0dce66e2e
Louie D <louie.delrosario97@gmail.com>
2026-05-25 12:06:35 -0700

    Started building out class structure

--- src/main/java/com/pluralsight/models/parts/AnalogStick.java
+++ src/main/java/com/pluralsight/models/parts/AnalogStick.java
@@ -1,5 +1,17 @@
 package com.pluralsight.models.parts;
 
-public class AnalogStick {
+public class AnalogStick extends Part
+{
+    private String stickType; // Joystick or CStick
+
+
+
+    public AnalogStick(String name, String color)
+    {
+        super(name, color);
+    }
+
+    // Getters
+    public String getStickType() { return stickType; }
 }
 
