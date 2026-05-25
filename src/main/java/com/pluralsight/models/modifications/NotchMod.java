5433abef79010bc77bcbe300f27f3e5d627d7246
Louie D <louie.delrosario97@gmail.com>
2026-05-25 14:35:20 -0700

    created getPrice() logic and basic class structures

--- src/main/java/com/pluralsight/models/modifications/NotchMod.java
+++ src/main/java/com/pluralsight/models/modifications/NotchMod.java
@@ -1,5 +1,38 @@
 package com.pluralsight.models.modifications;
 
-public class NotchMod {
+public class NotchMod extends Mod
+{
+    private String notchedTo;
+
+    private static final double joyStickPrice = 45.00;
+    private static final double cStickPrice = 45.00;
+    private static final double bothPrice = 75.00;
+
+
+
+    public NotchMod(String name, String notchedTo)
+    {
+        super("Stick Notch Mod");
+        this.notchedTo = notchedTo;
+    }
+
+    public String getNotchedTo() {
+        return notchedTo;
+    }
+
+    @Override
+    public double getPrice() {
+        switch (notchedTo.toLowerCase())
+        {
+            case "joystick":
+                return joyStickPrice;
+            case "c-stick":
+                return cStickPrice;
+            case "both":
+                return bothPrice;
+            default:
+                return 0.00;
+        }
+    }
 }
 
