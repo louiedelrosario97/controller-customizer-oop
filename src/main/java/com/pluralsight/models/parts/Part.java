b552a32ff5c11a7dfb1578be414cbde8f95407bc
Louie D <louie.delrosario97@gmail.com>
2026-05-25 12:07:16 -0700

    Building out abstract class for parts

--- src/main/java/com/pluralsight/models/parts/Part.java
+++ src/main/java/com/pluralsight/models/parts/Part.java
@@ -1,5 +1,42 @@
 package com.pluralsight.models.parts;
 
-public class Part {
+import com.pluralsight.interfaces.IPriceable;
+
+public abstract class Part implements IPriceable
+{
+    // Variables
+    String name;
+    String color;
+
+    // Constructor
+    public Part(String name, String color)
+    {
+        this.name = name;
+        this.color = color;
+    }
+
+    // Getters
+    public String getName() {
+        return name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+
+    // Setters
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+
+    // Abstract Method
+    @Override
+    public abstract double getPrice();
 }
 
