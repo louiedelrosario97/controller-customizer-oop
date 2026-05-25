2bfa28cd51bd4d41dd77f020bf67d3c8687756fe
Louie D <louie.delrosario97@gmail.com>
2026-05-25 15:12:30 -0700

    code cleaning

--- src/main/java/com/pluralsight/models/modifications/Mod.java
+++ src/main/java/com/pluralsight/models/modifications/Mod.java
@@ -4,16 +4,19 @@ import com.pluralsight.interfaces.IPriceable;
 
 public abstract class Mod implements IPriceable
 {
-    private String name;
+    // Mod Characteristic(s)
+    private final String name;
 
+    // Constructor
     public Mod(String name)
     {
         this.name = name;
     }
 
-    // Getters
+    // Getter
     public String getName() { return name; }
 
+    // Abstract Method
     @Override
     public abstract double getPrice();
 }
