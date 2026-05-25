c2e609ba4307d87ea813d6e7cd476392c608db4e
Louie D <louie.delrosario97@gmail.com>
2026-05-25 13:42:46 -0700

    Mod class is now abstract and Implements IPriceable

--- src/main/java/com/pluralsight/models/modifications/Mod.java
+++ src/main/java/com/pluralsight/models/modifications/Mod.java
@@ -1,4 +1,20 @@
 package com.pluralsight.models.modifications;
 
-public class Mod {  }
+import com.pluralsight.interfaces.IPriceable;
+
+public abstract class Mod implements IPriceable
+{
+    private String name;
+
+    public Mod(String name)
+    {
+        this.name = name;
+    }
+
+    // Getters
+    public String getName() { return name; }
+
+    @Override
+    public abstract double getPrice();
+}
 
