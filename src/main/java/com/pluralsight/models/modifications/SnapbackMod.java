028555430137582158a440c5463f2c8ed86aeb1e
Louie D <louie.delrosario97@gmail.com>
2026-05-25 13:44:18 -0700

    Created pricing for different axis types. Created Override logic for getPrice()

--- src/main/java/com/pluralsight/models/modifications/SnapbackMod.java
+++ src/main/java/com/pluralsight/models/modifications/SnapbackMod.java
@@ -1,5 +1,34 @@
 package com.pluralsight.models.modifications;
+// Snapback Capacitor Mod
+public class SnapbackMod extends Mod
+{
+    private String axis;
 
-public class SnapbackMod {
+    private static final double horizontalPrice = 50.00;
+    private static final double verticalPrice = 50.00;
+    private static final double bothPrice = 75.00;
+
+    public SnapbackMod(String axis)
+    {
+        super("Snapback Capacitor Mod");
+        this.axis = axis;
+    }
+
+    public String getAxis() { return axis; }
+
+    @Override
+    public double getPrice() {
+        switch (axis.toLowerCase())
+        {
+            case "horizontal":
+                return horizontalPrice;
+            case "vertical":
+                return verticalPrice;
+            case "both":
+                return bothPrice;
+            default:
+                return 0.00;
+        }
+    }
 }
 
