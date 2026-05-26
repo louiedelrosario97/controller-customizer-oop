b09a00b110083ce65c986b1618ca99c20420a4ec
Louie D <louie.delrosario97@gmail.com>
2026-05-26 12:53:18 -0700

    Creating Cart class, working on logic to add elements from different List<>

--- src/main/java/com/pluralsight/models/store/Cart.java
+++ src/main/java/com/pluralsight/models/store/Cart.java
@@ -1,7 +1,39 @@
 package com.pluralsight.models.store;
 
+import com.pluralsight.models.accessory.Accessory;
+import com.pluralsight.models.controller.Controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Cart
 {
+    // List 'Order' can access
+    private List<Controller> controllers;
+    private List<Accessory> accessory;
 
+    // Constructor
+    public Cart()
+    {
+        this.controllers = new ArrayList<>();
+        this.accessory = new ArrayList<>();
+    }
+
+    // Methods (Add 1 or more controller(s), accessories)
+    public void addController(Controller controller)
+    {
+
+    }
+
+    public void addAccessory(Accessory accessory)
+    {
+
+    }
+
+    // Derived Getter
+    public double getCartTotal()
+    {
+        double total = 0;
+    }
 }
 
