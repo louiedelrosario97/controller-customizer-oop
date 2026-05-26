e3e0f58e0a07d18ddc41ca2b7c27a2aac3198e41
Louie D <louie.delrosario97@gmail.com>
2026-05-26 11:31:21 -0700

    Changed name to Main and now we will be moving all the screens to its own class.

---
+++ src/main/java/com/pluralsight/ui/main/Main.java
@@ -0,0 +1,41 @@
+package com.pluralsight.ui.main;
+
+import java.util.Scanner;
+
+public class Main
+{
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args)
+    {
+        homeScreen();
+    }
+
+
+//--------------------------------------------------- homeScreen() ----------------------------------------------------
+
+
+//--------------------------------------------------- orderScreen() ----------------------------------------------------
+    public static void orderScreen()
+    {
+        String input;
+        boolean runningLoop = true;
+        while (runningLoop)
+        {
+            System.out.println("1) Build A Controller");
+            System.out.println("2) Accessory Catalog");
+            System.out.println("3) Checkout");
+            System.out.println("0) Cancel Order");
+            System.out.print("Select: ");
+wha
+        }
+    }
+//--------------------------------------------------- homeScreen() ----------------------------------------------------
+
+
+
+
+
+
+}
+
