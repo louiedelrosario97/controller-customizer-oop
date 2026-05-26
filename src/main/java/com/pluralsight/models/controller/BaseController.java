85d056ba74751ed12cf0b0c5461bfa81a4a1c2de
Louie D <louie.delrosario97@gmail.com>
2026-05-25 17:40:22 -0700

    created a BaseController class. This class does not extend Part, as it is the core of the object itself. It is required, unlike the other classes that are optional customizations.

---
+++ src/main/java/com/pluralsight/models/controller/BaseController.java
@@ -0,0 +1,24 @@
+package com.pluralsight.models.controller;
+
+import com.pluralsight.interfaces.IPriceable;
+// BaseController is not a Part, but still implements IPriceable
+public class BaseController implements IPriceable
+{
+    // Variables
+    private final String name;
+    private final double price;
+
+    // Constructor
+    public BaseController(String name, double price)
+    {
+        this.name = name;
+        this.price = price;
+    }
+
+    // Getter
+    public String getName() { return name; }
+
+    @Override
+    public double getPrice() { return price; }
+}
+
