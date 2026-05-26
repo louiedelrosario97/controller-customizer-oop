2da797beeb4458e0b6bc31e154a85ab4d4b2f899
Louie D <louie.delrosario97@gmail.com>
2026-05-25 17:42:35 -0700

    Added class structure, still working on getCost() logic

--- src/main/java/com/pluralsight/models/controller/Controller.java
+++ src/main/java/com/pluralsight/models/controller/Controller.java
@@ -3,14 +3,41 @@ package com.pluralsight.models.controller;
 import com.pluralsight.models.modifications.Mod;
 import com.pluralsight.models.parts.Part;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Controller
-    // Has-A BaseController, Has-A List of Parts, Has-A List of Mods
+    // [Controller] Has-A BaseController, Has-A List of Parts, Has-A List of Mods, Has-A id to map to Order id
 {
-    // Variables
+    // Controller Characteristics
+    private final BaseController base;
+    private final int id;
+
+    // Lists (Blueprint)
     private List<Part> parts;
     private List<Mod> mods;
-    private String BaseController;
+
+    public Controller(BaseController base, int id)
+    {
+        this.id = id;
+        this.base = base;
+        this.parts = new ArrayList<>();
+        this.mods = new ArrayList<>();
+    }
+
+    // Getters
+    public BaseController getBase() { return base; }
+    public int getId() { return id; }
+    public List<Part> getParts() { return parts; }
+    public List<Mod> getMods() { return mods; }
+
+    // Derived Getters
+    public double getCost()
+    {
+        double cost = 0;
+        cost += base.getPrice();
+        // Add rest of logic later ~
+        return cost;
+    }
 }
 
