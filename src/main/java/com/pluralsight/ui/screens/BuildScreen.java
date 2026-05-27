21e3cd931e717b748ff3207edf0d6a3092d16b70
Louie D <louie.delrosario97@gmail.com>
2026-05-26 19:16:58 -0700

    Brainstorming user flow for build screen

--- src/main/java/com/pluralsight/ui/screens/BuildScreen.java
+++ src/main/java/com/pluralsight/ui/screens/BuildScreen.java
@@ -1,5 +1,8 @@
 package com.pluralsight.ui.screens;
 
+import com.pluralsight.models.controller.BaseController;
+import com.pluralsight.models.controller.Controller;
+
 import java.util.Scanner;
 
 // This screen will guide through the custom-building of a controller. When completing the build,
@@ -8,7 +11,28 @@ public class BuildScreen
 {
     private static Scanner scanner = new Scanner(System.in);
 
-    public static Controller start
+    public static Controller buildStart()
+    {
+        System.out.println("\n   Design Your Controller   ");
+        System.out.println("----------------------------------");
+
+        // 1. Select a base controller ( BaseController )
+        BaseController base =
+        // 2. Select paint color
+
+        // 3. Select custom button set
+
+        // 4. Select custom joystick
+
+        // 5. Select custom c-stick
+
+        // 6. Select snapbackmod
+
+        // 7. Select TactileZMod
+
+    }
+
+    p
 
 }
 
