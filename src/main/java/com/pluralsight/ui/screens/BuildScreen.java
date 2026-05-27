7ca127e35715bf1324eebacb88a7803b4291d8ee
Louie D <louie.delrosario97@gmail.com>
2026-05-26 23:54:51 -0700

    Created logic for the first 2 steps in the BuildScreen. Built a for loop to print the 'bases' List for the user to select from. For ShellColor we will build similar logic but will need to use a switch case since it is an optional component. This will be generally how the rest of the logic for this screen will look.

--- src/main/java/com/pluralsight/ui/screens/BuildScreen.java
+++ src/main/java/com/pluralsight/ui/screens/BuildScreen.java
@@ -2,7 +2,9 @@ package com.pluralsight.ui.screens;
 
 import com.pluralsight.models.controller.BaseController;
 import com.pluralsight.models.controller.Controller;
+import com.pluralsight.utilities.CatalogData;
 
+import java.util.List;
 import java.util.Scanner;
 
 // This screen will guide through the custom-building of a controller. When completing the build,
@@ -11,28 +13,47 @@ public class BuildScreen
 {
     private static Scanner scanner = new Scanner(System.in);
 
-    public static Controller buildStart()
+    public static void start()
     {
         System.out.println("\n   Design Your Controller   ");
         System.out.println("----------------------------------");
 
-        // 1. Select a base controller ( BaseController )
-        BaseController base =
-        // 2. Select paint color
+        // Step 1: Base Controller
+        System.out.println("\n   Select base controller :");
 
-        // 3. Select custom button set
+        List<BaseController> bases = CatalogData.getBaseControllers();
+        for (int i = 0; i < bases.size(); i++)
+        {
+            System.out.println((i + 1) + ") " + bases.get(i));
+        }
+        System.out.print("Select: ");
+        int baseSelection = scanner.nextInt();
 
-        // 4. Select custom joystick
+        BaseController base = bases.get(baseSelection - 1);
 
-        // 5. Select custom c-stick
+        Controller controller = new Controller(base);
 
-        // 6. Select snapbackmod
+        // 2: ShellColor
+        System.out.println("\n Would you to custom paint your controller? (+$50) (Y/N): ");
+        String input = scanner.nextLine();
 
-        // 7. Select TactileZMod
-
+        switch (input.toLowerCase())
+        {
+            case "y": List<String> colors = CatalogData.getShellColor();
+            case "n":
+        }
     }
 
-    p
 
+
+    // 3. Select custom button set
+
+    // 4. Select custom joystick
+
+    // 5. Select custom c-stick
+
+    // 6. Select snapback mod
+
+    // 7. Select TactileZMod
 }
 
